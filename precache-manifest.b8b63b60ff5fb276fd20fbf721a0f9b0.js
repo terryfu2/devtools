426f05e22404a851578a395ab1d768ca761b1d2a
Terry Fu <terryfu33@gmail.com>
2024-05-12 16:07:01 -0600

    Updates

---
+++ precache-manifest.b8b63b60ff5fb276fd20fbf721a0f9b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2d56015939e6ebd31f6b667d87c611",
+    "url": "/devtools/index.html"
+  },
+  {
+    "revision": "0fa2528b3f1e4fdf8ccd",
+    "url": "/devtools/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "44543a7228bbd3ea8197",
+    "url": "/devtools/static/css/main.c3cb5dea.chunk.css"
+  },
+  {
+    "revision": "0fa2528b3f1e4fdf8ccd",
+    "url": "/devtools/static/js/2.648a8a12.chunk.js"
+  },
+  {
+    "revision": "a27870aba77b083fc6b4a9feb3ebcdea",
+    "url": "/devtools/static/js/2.648a8a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44543a7228bbd3ea8197",
+    "url": "/devtools/static/js/main.423b2fdd.chunk.js"
+  },
+  {
+    "revision": "ccf756c1b0711a56bdad",
+    "url": "/devtools/static/js/runtime-main.d54a82b8.js"
+  }
+]);
