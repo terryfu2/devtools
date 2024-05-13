d79f179759bb312b2aa4907790095ae84e27e027
Terry Fu <terryfu33@gmail.com>
2024-05-13 15:48:27 -0600

    Updates

--- precache-manifest.6cce898808952c9cc538317da09eefa4.js
+++ precache-manifest.6cce898808952c9cc538317da09eefa4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "256c1ab0301365f3b2355d50d7631405",
+    "revision": "6df862c815c4ad413088fd626364bb43",
     "url": "/devtools/index.html"
   },
   {
-    "revision": "9ec4bbc7300d19150ecf",
+    "revision": "ecc0c36a6041d641875c",
     "url": "/devtools/static/css/2.98036eff.chunk.css"
   },
   {
-    "revision": "497fe5b5a683ec840c20",
+    "revision": "821de5231ce36d3d1f98",
     "url": "/devtools/static/css/main.c3cb5dea.chunk.css"
   },
   {
-    "revision": "9ec4bbc7300d19150ecf",
-    "url": "/devtools/static/js/2.f798801d.chunk.js"
+    "revision": "ecc0c36a6041d641875c",
+    "url": "/devtools/static/js/2.4095eb1c.chunk.js"
   },
   {
     "revision": "a27870aba77b083fc6b4a9feb3ebcdea",
-    "url": "/devtools/static/js/2.f798801d.chunk.js.LICENSE.txt"
+    "url": "/devtools/static/js/2.4095eb1c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "497fe5b5a683ec840c20",
-    "url": "/devtools/static/js/main.17a6222a.chunk.js"
+    "revision": "821de5231ce36d3d1f98",
+    "url": "/devtools/static/js/main.c29d5b65.chunk.js"
   },
   {
     "revision": "ccf756c1b0711a56bdad",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/devtools/precache-manifest.615b64f1c916d45c1d02c9b09527f940.js"
+  "/devtools/precache-manifest.6cce898808952c9cc538317da09eefa4.js"
 );
 
 self.addEventListener('message', (event) => {
